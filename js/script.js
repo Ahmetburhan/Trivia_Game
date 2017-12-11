ce3701533fde79b2a15481ae84b96b47c4b826d8
AhmetK <ahmetburhan@gmail.com>
2017-12-10 16:32:48 -0800

    modal created

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// <!-- Make header line
+// Create DIV that shows start button
+// Create Div enables to show after press of Start
+// Set timer
+// Create Random generated questions
+// Answers to be selected
+// Make a case if answers is wrong or not selected right Display results.
+// Make an early completion button saying Done or Submit
+// Show Numbers of correct answer, incorrect answers and show unanswered questions.
+// MAke a refresh page button.
+//  -->
+
+  $(document).ready(function(){
+    $("#myModal").modal('show');
+  });
